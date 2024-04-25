2991ef8ca7e014c42988ceb23b8f522d58ede76d
jasonli5 <jasonli5@andrew.cmu.edu>
2024-04-25 12:37:06 -0400

    Add sklearn import statement to Winemaker_part3.py

--- Winemaker_part3.py
+++ Winemaker_part3.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
